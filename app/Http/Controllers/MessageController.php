2dd271159e7eedd68be6e11f22ff2e21338ff2e1
Roberto Gonzalez Arriaga <htk.rgl@gmail.com>
2020-10-20 08:21:43 -0500

    Validando Formularios

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -8,6 +8,12 @@ class MessageController extends Controller
 {
     //public function store(Request $request){ Utilizamos el metodo request y lo guardamos en una variable
 	public function store(){
-    	return request();//Funcion request de la instancia Illuminate
+    	//return request();//Funcion request de la instancia Illuminate
+    	request()->validate([
+    		'name' => 'required',
+    		'email' => 'required|email',//['required','email']
+    		'subject' => 'required',
+    		'content' => 'required',
+    	]);
 	}
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,10 +5,14 @@
 	<h1>Contact</h1>
 	<form method="POST" action="{{ route('contact') }}">
 		@csrf
-		<input name="name" placeholder="Nombre....">
-		<input name="email" type="email" placeholder="Email....">
-		<input name="subject" placeholder="Asunto....">
-		<textarea name="content" placeholder="Mensaje..."></textarea>
+		<input name="name" placeholder="Nombre...." value="{{ old('name') }}"><br>
+		<p>{{ $errors->first('name') }}</p>
+		<input name="email" type="email" placeholder="Email...." value="{{ old('email') }}"><br>
+		<p>{{ $errors->first('email') }}</p>
+		<input name="subject" placeholder="Asunto...." value="{{ old('subject') }}"><br>
+		<p>{{ $errors->first('subject') }}</p>
+		<textarea name="content" placeholder="Mensaje..." value="{{ old('content') }}"></textarea><br>
+		<p>{{ $errors->first('content') }}</p>
 		<button>Enviar</button>
 	</form>
 @endsection
